0b3260beed3a9c50af892f17b4e4de573518d778
CapCherif <gharbi_cherif@hotmail.com>
2024-09-21 11:59:49 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+
+
+var menu = [
+    {
+        nom:"Pizza Fromage",
+        prix:450,
+        img:"imgs/fromage.jfif",
+        category:"pizza"
+    },
+    {
+        nom:"Pizza Italienne",
+        prix:500,
+        img:"imgs/italienne.jfif",
+        category:"pizza"
+    },
+    {
+        nom:"Pizza Saumon",
+        prix:450,
+        img:"imgs/saumon.jfif",
+        category:"pizza"
+    },
+    {
+        nom:"Chicken",
+        prix:450,
+        img:"imgs/chicken.jfif",
+        category:"burger"
+    },
+    {
+        nom:"Boeuf",
+        prix:450,
+        img:"imgs/boeuf.jfif",
+        category:"burger"
+    },
+    {
+        nom:"Shawarma",
+        prix:450,
+        img:"imgs/shawarma.jfif",
+        category:"burger"
+    },
+]
+
+
+function CreateMenu(category){
+
+    var html = ""
+    menu.forEach((m)=>{
+        if(m.category == category){
+            html += `
+            <div class="menu">
+                <img src="${m.img}" alt="">
+                <h4>${m.nom}</h4>
+                <p>${m.prix} DA</p>
+            </div>
+            `
+        }
+        
+    })
+
+    var resultat = document.querySelector('#resultat')
+    resultat.innerHTML = html
+}
+
+// au démarrage
+
+CreateMenu("pizza")
+
+
+var button = document.querySelector('button')
+
+button.addEventListener("click", function(){
+    var category = document.querySelector('select').value
+    CreateMenu(category)
+})
